9cb03797918a74e3f2ee2e3befe2fec015641742
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-10 13:12:02 +0530

    Removes duplicate characters in string

---
+++ DuplicateChar/src/duplicatechar.java
@@ -0,0 +1,29 @@
+//import java.util.Scanner;
+class duplicatechar {
+	public static void main(String arg[]) {
+		//Scanner sc = new Scanner(System.in);
+		String str = "aabbcaef";
+		char arr[] = str.toCharArray();
+		for(char i:arr) {
+			System.out.println(" "+i);
+		}
+		int size = str.length();
+		System.out.println("size = "+size);
+		char temp[] = new char[size];
+		int length = 0;   
+		int i,j=0;
+		for(i = 0; i < size-1; i++) {     
+			if(arr[i] != arr[i+1]) {
+				temp[j++] = arr[i];
+			}
+		}
+		temp[j++] = arr[size-1];
+//		for(i = 0; i < j; i++) {
+//			arr[i] = temp[i];
+//		}
+		length = j;
+		for(i = 0; i < length; i++) {
+			System.out.print(" "+temp[i]);
+		}
+}
+}
