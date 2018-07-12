4937a18c7d5a2ed6e5b02548b92487f38cbccc9b
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-12 19:30:44 +0530

    remove continuous duplicate number from an array

---
+++ DuplicateNo/src/duplicate.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class duplicate {
+	public static void main(String arg[]) {
+	Scanner sc = new Scanner(System.in);
+	int size=5,length=0;
+	int i;
+	int arr[] = {2,2,3,4,5};
+//	System.out.println("Enter Array Size : ");
+//	size = sc.nextInt();
+//	int arr[] = new int[size];
+//	System.out.println("Enter Array = ");
+	//for(i = 0; i < size; i++) {
+//		arr[i] = sc.nextInt();
+//	}
+	int temp[] = new int[size];
+	 if (size==0 || size==1){  
+         length = size;  
+     }    
+	 int j = 0;
+     for ( i=0; i < size-1; i++){  
+         if (arr[i] != arr[i+1]){  
+             temp[j++] = arr[i];  
+         }  
+      }  
+     temp[j++] = arr[size-1];        
+     // Changing original array  
+     for (i=0; i < j; i++){  
+         arr[i] = temp[i];  
+     }
+     length = j; 
+     System.out.println("New Array = ");
+     for(i = 0; i < length; i++) {
+    	 System.out.print(arr[i]+" ");
+     }
+ }  
+}
