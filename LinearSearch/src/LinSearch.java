7dc86aa44c3eca384018bfb0d327bccc9e976695
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-17 23:47:49 +0530

    Linear Search in user-defined array

---
+++ LinearSearch/src/LinSearch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class LinSearch {
+	public static void main(String arg[]) {
+		Scanner sc = new Scanner(System.in);
+		int size;
+		int i;
+		System.out.println("Enter Size : ");
+		size = sc.nextInt();
+		int arr[] = new int[size];
+		System.out.println("Enter Elements : ");
+		for(i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int num;
+		System.out.println("Enter number To Search : ");
+		num = sc.nextInt();
+		for(i = 0; i < size; i++) {
+			if(arr[i] == num) {
+				System.out.println("Element Found at Position "+(i+1));
+			}
+			}
+	}
+
+}
