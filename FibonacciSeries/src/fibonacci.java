0f1b87f3d315601007277b720bcb51f1732744f0
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-07-26 19:40:45 +0530

    Printing Fibonacci Series upto given range.

---
+++ FibonacciSeries/src/fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class fibonacci {
+	public static void main(String arg[]) {
+	Scanner sc = new Scanner(System.in);
+	int n1=0, n2=1, n3, len;
+	int i;
+	System.out.println("Enter Input = ");
+	len = sc.nextInt();
+	System.out.print(n1 +" "+n2);
+	for(i = 2; i < len; i++) {
+		if(len > 0) {
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+			System.out.print(" "+n3);
+		}
+	}
+}
+}
