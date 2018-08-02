ea4ae331e0131c653f1cde893e76ed627904cf5b
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-02 19:46:07 +0530

    Smallest element in three sorted arrays.

--- SmallestAmong3Array/src/smallest.java
+++ SmallestAmong3Array/src/smallest.java
@@ -1,14 +1,18 @@
-public class common {
+public class smallest {
 	public static void find_smallest(int ar1[], int ar2[], int ar3[]) {
 		int i = 0;
 		int j = 0;
 		int k = 0;
+		int z =0;
+		int temp[] = new int[30];
 		while(i < ar1.length && j < ar2.length && k < ar3.length) {
 			if(ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
-				System.out.print(ar1[i]+" ");
+				temp[z] = ar1[i];
+				System.out.println(temp[z]+" ");
 				i++;
 				j++;
 				k++;
+				z++;
 			}
 			else if(ar1[i] < ar2[j]) {
 				i++;
@@ -20,10 +24,10 @@ public class common {
 				k++;
 			}
 		}
+		System.out.println("Smallest Element : "+temp[0]);
 }
 	
 	public static void main(String args[]) {
-		//smallest ob = new smallest();
 		int ar1[] = {1,2,4,5,6,7};
 		int ar2[] = {2,4,5,6,7,8};
 		int ar3[] = {2,4,6,8,9,10,12};
