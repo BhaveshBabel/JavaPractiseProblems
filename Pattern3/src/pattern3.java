da28a242fcc417aceb022d9032a8fcd0004f765e
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-13 18:32:22 +0530

    Program to print stars in form of triangle.

---
+++ Pattern3/src/pattern3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of Rows : ");
+		int rows = sc.nextInt();
+		int i, j, k;
+		for(i = 0; i < rows; i++) {
+			for(j = i; j < rows; j++) {
+				System.out.print(" ");
+			}
+			for(k = 0; k <= i; k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
