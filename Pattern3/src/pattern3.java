c43932752cc14f2d4a7118f30c2aef8346b1b5f4
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-15 17:47:09 +0530

    Mirrored right triangle star pattern.

--- Pattern3/src/pattern3.java
+++ Pattern3/src/pattern3.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class pattern3 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter no of Rows : ");
 		int rows = sc.nextInt();
---
+++ Pattern5/src/pattern5.java
@@ -0,0 +1,28 @@
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+import java.util.Scanner;
+public class pattern5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i, j, k;
+		System.out.println("Enter No of Rows : ");
+		int rows = sc.nextInt();
+		for(i = 0; i < rows; i++) {
+			for(j = i; j < rows-1 ; j++) {
+				//Print spaces in decreasing order of row
+				System.out.print(" ");
+			}
+			for(k = 0; k <= i ; k++) {
+				//Print star in increasing order or row
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
+
