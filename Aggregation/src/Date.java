ea8c9a8ac8bb5685d300465efd8bbcd6da1040a8
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-19 19:40:46 +0530

    Example of Java Aggregation.

---
+++ Aggregation/src/Date.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Date {
+	int dd,mm,yy;
+	void setDate(int d, int m, int y) {
+		dd = d;
+		mm = m;
+		yy = y;
+	}
+	void showDate() {
+		System.out.println(dd+"/"+mm+"/"+yy);
+	}
+}
+class Person{
+	int id;
+	String name;
+	Date d ;
+	void setData(int i, String n, int d1, int m, int y) {
+		id = i;
+		name = n;
+		d = new Date();
+		d.setDate(d1,m,y);
+	}
+	void printInfo() {
+		System.out.println("ID = "+id);
+		System.out.println("NAME = "+name);
+		d.showDate();
+	}
+}
+class Final{
+	public static void main(String arg[]) {
+		int I,D,M,Y;
+		String N;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Id = ");
+		I = sc.nextInt();
+		System.out.println("Name = ");
+		N = sc.next();
+		System.out.println("Date = ");
+		D = sc.nextInt();
+		System.out.println("Month = ");
+		M = sc.nextInt();
+		System.out.println("Year = ");
+		Y = sc.nextInt();
+		Person p = new Person();
+		p.setData(I, N, D, M, Y);
+		p.printInfo();
+		
+		sc.close();
+	}
+}
