4fe7e45ff6d6307f95d24e3abab10f04c78312ef
BhaveshBabel <bhaveshbabel86@gmail.com>
2018-08-23 19:44:27 +0530

    Method Overriding example.

---
+++ MethodOverriding/src/Example.java
@@ -0,0 +1,21 @@
+class override{
+	void print() {
+			System.out.println("In Base class");
+		}
+	}
+
+	class child extends override{
+		void print() {
+			System.out.println("In Child class");
+		}
+	}
+	
+	class Example{
+		public static void main(String arg[]){
+			override B = new override();
+			child C = new child();
+			B.print();
+			C.print();
+		}
+	}
+
